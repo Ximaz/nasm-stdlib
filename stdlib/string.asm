0b007701de2d6d29342322110582e842c6228691
quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2022-04-21 00:09:02 +0200

    .

--- stdlib/string.asm
+++ stdlib/string.asm
@@ -4,15 +4,15 @@ BITS 64
 ; (unsigned int: rcx) strlen(char *buffer: rsi);
 ; --------------------------------------------------------------------------------
 strlen:
-    xor rcx, rcx               ; unsigned int rcx = 0;
-__strlen_counter_start:        ; while (1) {
-    inc rcx                    ;     rcx++
-    cmp byte [rsi+rcx], 0      ;     if (rsi[rcx] != 0)
-    jnz __strlen_counter_start ;         continue;
-__strlen_counter_end:          ;
-    dec rcx                    ;     rcx--;
-    ret                        ;     break;
-                               ; }
+    xor rcx, rcx                     ; unsigned int rcx = 0;
+__strlen_counter_start:              ; while (1) {
+    inc rcx                          ;     rcx++
+    cmp byte [rsi+rcx], 0            ;     if (rsi[rcx] != 0)
+    jnz short __strlen_counter_start ;         continue;
+__strlen_counter_end:                ;
+    dec rcx                          ;     rcx--;
+    ret                              ;     break;
+                                     ; }
 
 ; --------------------------------------------------------------------------------
 ; (char *substring: rax) substr(char *buffer: rsi,
@@ -22,5 +22,5 @@ __strlen_counter_end:          ;
 substr:
     mov rax, rsi          ; rax = rsi
     add rax, rdx          ; rax = rax[rcx:]
-    mov [rax+rcx], word 0 ; rax = rax[:rcx+rdx]
+    mov byte [rax+rcx], 0 ; rax = rax[:rcx+rdx]
     ret
