26bdfa7edc2e0ccc314ab6d7b4d684714464d7c4
quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2022-04-21 14:57:20 +0200

    .

--- stdlib/string.asm
+++ stdlib/string.asm
@@ -4,19 +4,16 @@ BITS 64
 ; (__INT: rcx) strlen(__CHAR *buffer: rsi);
 ; --------------------------------------------------------------------------------
 strlen:
-    xor rcx, rcx                     ; __INT rcx = 0;
-__strlen_counter_start:              ; while (1) {
-    inc rcx                          ;     rcx++
-    cmp byte [rsi+rcx], 0            ;     if (rsi[rcx] != 0)
-    jnz short __strlen_counter_start ;         continue;
-__strlen_counter_end:                ;
-    retn                             ;     break;
-                                     ; }
+    xor rcx, rcx                    ; __INT rcx = 0;
+__strlen_counter:                   ; while (1) {
+    inc rcx                         ;     rcx++
+    cmp byte [rsi+rcx], 0           ;     if (rsi[rcx] != 0)
+    jnz short __strlen_counterstart ;         continue;
+    retn                            ;     break;
+                                    ; }
 
 ; --------------------------------------------------------------------------------
-; (__CHAR *substring: rax) substr(__CHAR *buffer: rsi,
-;                               __INT start: rcx,
-;                               __INT span: rdx);
+; (__CHAR *substring: rax) substr(__CHAR *buffer: rsi, __INT start: rcx, __INT span: rdx);
 ; --------------------------------------------------------------------------------
 substr:
     mov rax, rsi          ; rax = rsi
