aeae2580cc6cc904e3a8d896cb2fda92b386c6c7
quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2022-04-21 12:50:07 +0200

    .

--- main.asm
+++ main.asm
@@ -3,16 +3,20 @@ BITS 64
 %include "stdlib/core.asm"
 
 section .data
+    debug_stage db "DEBUG", 0
     data db "This is my file content", 0
 
 section .bss
     filename_length equ __UCHAR * 10
     filename resb filename_length
+    data_length equ __UCHAR * 24
+    file_data resb data_length
 
 section .text
     global _start
 
 _start:
+create_file:
     ; Read filename
     mov rsi, filename
     mov rdx, filename_length
@@ -25,12 +29,29 @@ _start:
     call open
 
     ; Write the data into the file.
-    lea rsi, [data]
+    mov rsi, data
     call write
 
     ; Close the file.
     call close
+read_file:
+    ; Open the file in read-only mode.
+    mov rdi, filename
+    mov rsi, O_RDONLY
+    call open
 
+    ; Read the file
+    mov rsi, file_data
+    mov rdx, data_length
+    call read
+
+    ; Close the file
+    call close
+
+print:
+    ; Print the file data.
+    call stdout
+return:
     ; Exit the program
 	xor dl, dl
 	call exit
